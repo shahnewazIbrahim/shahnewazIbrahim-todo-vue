2c455513f80b9f4f27e107c991f31e6be879455c
Mamba-dot <shahnewaz886@gmail.com>
2021-10-13 13:43:14 +0600

    message

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <Header />
 
     <AddTodo @add-todo="addTodo" />
+    <p id="message"></p>
 
     <Todos v-bind:todos="todos" @del-todo="deleteTodo" /> 
   </div>
@@ -28,17 +29,35 @@ export default {
   },
   methods: {
     deleteTodo(id) {
-      axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
-      this.todos = this.todos.filter(todo => todo.id !== id)
+      axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`);
+      this.todos = this.todos.filter(todo => todo.id !== id);
     },
     addTodo(newTodo) {
-      const {title, completed} = newTodo
-      axios.post('https://jsonplaceholder.typicode.com/todos',{
-        title,
-        completed
-      })
-      .then(response=> this.todos = [...this.todos, response.data])
-      .catch(error=> console.log(error))
+      const {title, completed} = newTodo;
+
+      console.log(title);
+
+      if(title != '') {
+        document.getElementById('message').style.display = "none";
+        document.getElementById('message').innerHTML = "";
+
+        axios.post('https://jsonplaceholder.typicode.com/todos',{
+          title,
+          completed
+        })
+        .then(response=> this.todos = [...this.todos, response.data])
+        .catch(error=> console.log(error))
+
+      }else {
+        document.getElementById('message').style.display = "";
+        document.getElementById('message').innerHTML = "Please fill the text field";
+
+        setInterval(function(){
+          document.getElementById('message').style.display = "none";
+          document.getElementById('message').innerHTML = "";
+        }, 2000)
+
+      }
       
     },
   },
@@ -60,5 +79,11 @@ export default {
  body {
    font-family: Arial, Helvetica, sans-serif;
    line-height: 1.4;
+ }
+
+ #message {
+   background: tomato;
+   color: #fff;
+   text-align: center;
  }
 </style>
