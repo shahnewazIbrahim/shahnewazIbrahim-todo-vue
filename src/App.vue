53b21add6dc84540f83ad5fd301e9d86a977e332
Mamba-dot <shahnewaz886@gmail.com>
2021-10-13 11:26:41 +0600

    new vue app todo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,64 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="flex ">
+    <Header />
+
+    <AddTodo @add-todo="addTodo" />
+
+    <Todos v-bind:todos="todos" @del-todo="deleteTodo" /> 
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todos from "./components/Todos.vue";
+import Header from "./components/layout/Header.vue";
+import AddTodo from "./components/AddTodo.vue";
+import axios from "axios";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Todos,
+    Header,
+    AddTodo
+  },
+  data(){
+    return {
+      todos : []
+    }
+  },
+  methods: {
+    deleteTodo(id) {
+      axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+      this.todos = this.todos.filter(todo => todo.id !== id)
+    },
+    addTodo(newTodo) {
+      const {title, completed} = newTodo
+      axios.post('https://jsonplaceholder.typicode.com/todos',{
+        title,
+        completed
+      })
+      .then(response=> this.todos = [...this.todos, response.data])
+      .catch(error=> console.log(error))
+      
+    },
+  },
+  created() {
+      axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
+        .then(response=> this.todos = response.data)
+        .catch(error=> console.log(error))
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+ * {
+   box-sizing: border-box;
+   margin: 0;
+   padding: 0;
+ }
+
+ body {
+   font-family: Arial, Helvetica, sans-serif;
+   line-height: 1.4;
+ }
 </style>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,24 @@
+<template>
+    <header class="header">
+        <h3>TodoList</h3>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style scoped>
+    .header {
+        background: #333;
+        color: #fff;
+        text-align: center;
+        padding: 10px;
+    }
+    .header a {
+        color: #fff;
+        padding-right: 5px;
+    }
+</style>
